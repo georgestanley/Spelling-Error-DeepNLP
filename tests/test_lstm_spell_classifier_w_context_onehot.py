7f5edea93b2df5acf0777fbb386cb230e5ff928c
Stanley George <stanley@constellr.space>
2022-07-28 20:01:14 +0200

    new test cases

--- tests/test_lstm_spell_classifier_w_context_onehot.py
+++ tests/test_lstm_spell_classifier_w_context_onehot.py
@@ -30,7 +30,6 @@ class Test_lstm_w_context_onehot(TestCase):
         np.testing.assert_array_equal(get_wikipedia_text('test_file.jsonl', lower_case=True), test_array)
         return
 
-
     def test_get_bea60_data(self):
         test_file_path = 'bea60k_sentences_test_file.json'
 
@@ -64,24 +63,96 @@ class Test_lstm_w_context_onehot(TestCase):
                                       np.array(['Can I be  at office', 'will you be there ']))
 
     def test_cleanup_data(self):
-        pass
+        np.testing.assert_array_equal(cleanup_data(np.array(['Can, I be ! at office.', 'will you be there ?'])),
+                                      np.array(['Can I be  at office', 'will you be there ']))
 
     def test_generate_N_grams(self):
-        pass
+        dataset_old = np.array(['i hope you will be there',
+                                'the sun is yellow and round'])
+        dataset_new = (np.array([
+            'i hope you will be',
+            'hope you will be there',
+            'the sun is yellow and',
+            'sun is yellow and round']),
+                       np.array([0, 0, 0, 0]))
+        np.testing.assert_array_equal(generate_N_grams(dataset_old)[0], dataset_new[0])
+        np.testing.assert_array_equal(generate_N_grams(dataset_old)[1], dataset_new[1])
 
     def test_generate_N_grams_valdata(self):
-        pass
+        dataset_old = (
+            np.array(['WANT TO THANK YOU FOR', "In our Acadeemy we are"]),
+            np.array([0, 1])
+        )
+        # new_d , labels = generate_N_grams_valdata(dataset_old)
+        dataset_new = (np.array([
+            'WANT TO THANK YOU FOR',
+            'In our Acadeemy we are'
+        ]),
+                       np.array([0, 1])
+        )
+        # self.assertEqual(generate_N_grams_valdata(dataset_old)[0], dataset_new[0])
+        np.testing.assert_array_equal(generate_N_grams_valdata(dataset_old)[0], dataset_new[0])
+        np.testing.assert_array_equal(generate_N_grams_valdata(dataset_old)[1], dataset_new[1])
 
     def test_convert_to_pytorch_dataset(self):
-        pass
+        input_1 = (np.array(['hope you will be there',
+                    'i cannot maze it tonight']), np.array([0, 1]))
+        input_2 = (np.array(['hope you will be there',
+                    'i cannot maze it tonight']), np.array([0, 1]))
+
+        dataset_size = len(input_1)
+        dataloader_1, dataloader_2 = convert_to_pytorch_dataset(train_data=input_1, val_data=input_2, batch_size=1)
+        self.assertEqual(len(dataloader_1.dataset), dataset_size)
+        self.assertEqual(len(dataloader_2.dataset), dataset_size)
+        self.assertIsInstance(dataloader_1, torch.utils.data.dataloader.DataLoader)
+        self.assertIsInstance(dataloader_2, torch.utils.data.dataloader.DataLoader)
+
+        for i, data in enumerate(dataloader_1):
+            if i==0:
+                np.testing.assert_array_equal(data[0],np.array(['hope you will be there']))
+                torch.testing.assert_close(data[1],torch.tensor([0]))
+            if i==1:
+                np.testing.assert_array_equal(data[0], np.array(['i cannot maze it tonight']))
+                torch.testing.assert_close(data[1], torch.tensor([1]))
+
+        for i, data in enumerate(dataloader_2):
+            if i==0:
+                np.testing.assert_array_equal(data[0],np.array(['i cannot maze it tonight','hope you will be there',]))
+                torch.testing.assert_close(data[1],torch.tensor([1,0]))
+            if i==1:
+                self.assert_(True,'Dataset Size Exceeded. Check !')
+
 
     def test_initialize_model(self):
-        pass
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        model, criterion, optimizer = initialize_model(hidden_dim=100, hidden_layers=2, lr=0.001, device=device)
+        self.assertIsInstance(model, torch.nn.parallel.DataParallel)
+        self.assertIsInstance(criterion, torch.nn.modules.loss.CrossEntropyLoss)
+        self.assertIsInstance(optimizer, torch.optim.Adam)
+
+        output = model(torch.rand(61, 60,77), [10]*61)
+        self.assertEqual(output.shape, torch.Size([61, 2]))
 
     def test_insert_errors(self):
-        pass
+        dataset_old =(
+            'i hope you will be','I will make something today'
+        )
+        dataset_new = ['i hope you will be', 'I will make something today', 'i hope yu will be', 'I will mak '
+                                                                                                 'something today']
+
+        self.assertEqual(insert_errors(dataset_old)[0], dataset_new)
+        self.assertEqual(insert_errors(dataset_old)[1], [0,0,1,1])
 
     def test_one_hot_encode_data(self):
+        dataset_old = (
+            'x x Stanley x x', 'y y Stephin y y'
+        )
+        labels = torch.tensor([0, 0])
+        op,l,arr_len = one_hot_encode_data(dataset_old,with_error=True,labels=labels, shuffle=True, maxlen=60)
+        torch.testing.assert_close(op[0][0][49],torch.tensor(1)) #x
+        torch.testing.assert_close(op[0][1][76],torch.tensor(1)) #(space)
+        torch.testing.assert_close(op[0][2][49],torch.tensor(1)) #x
+        self.assertEqual(op.shape,torch.Size([4,60,77]))
         pass
 
     def test_train_model(self):
