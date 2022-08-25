3fbc5ee2b558a10dab20a674a5ab2b0e387cec25
Stanley George <stanley@constellr.space>
2022-08-25 15:20:48 +0200

    chnages

--- application/run_tests.sh
+++ application/run_tests.sh
@@ -1,3 +1,5 @@
 python -m application.tests.test_lstm_spell_classifier_w_context
 python -m application.tests.test_lstm_spell_classifier_w_context_onehot
 python -m application.tests.test_lstm_spell_classifier_wo_context
+python -m application.tests.test_utils
+python -m application.tests.test_dash_app
